07a0748df933527776e55e430a17b6246cf026f8
Tdanks2000 <tommydanks2000@hotmail.com>
2023-01-27 22:12:38 +0000

    fix star

--- src/pages/api/image/og/index.js
+++ src/pages/api/image/og/index.js
@@ -114,7 +114,7 @@ export default async function handler(req, res) {
               class="animeTitle"
               style={{
                 color: "white",
-                fontSize: "55px",
+                fontSize: "50px",
                 width: "90%",
                 marginBottom: "4px",
                 marginBottom: "0",
@@ -140,26 +140,25 @@ export default async function handler(req, res) {
               className="ratingWrapper"
               style={{
                 display: "flex",
-                fontSize: "48px",
+                fontSize: "45px",
                 alignItems: "center",
                 marginTop: "auto",
-                gap: "10px",
-                marginBottom: "8px",
+                gap: "15px",
+                marginBottom: "15px",
                 fontWeight: "600",
                 color: "white",
+                fill: color,
               }}
             >
-              {/* <svg
-                width="48px"
-                height="48px"
+              <svg
+                width="55px"
+                height="55px"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 576 512"
-                style={{
-                  fill: "currentColor",
-                }}
+                fill={color}
               >
                 <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-              </svg> */}
+              </svg>
               <span>9.7</span>
             </div>
             <div
@@ -177,7 +176,7 @@ export default async function handler(req, res) {
                   style={{
                     background: "rgba(80, 80, 80, 0.4)",
                     padding: "4px 15px",
-                    fontSize: "28px",
+                    fontSize: "24px",
                     borderRadius: "50px",
                     color: "white",
                     fontFamily: "Oswald-Regular",
