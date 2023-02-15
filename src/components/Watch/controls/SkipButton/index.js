99cbc3bba56ca394c4e1cb44842839016e27e171
Tommy Danks <tommydanks2000@hotmail.com>
2023-02-15 13:31:24 +0000

    add skip times

---
+++ src/components/Watch/controls/SkipButton/index.js
@@ -0,0 +1,63 @@
+import { useMediaPlayer, useMediaStore } from "@vidstack/react";
+import React, { useEffect, useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
+import { SkipButtonContainer } from "./SkipButton.styles";
+
+const SkipButton = (props) => {
+  const [opSkipTime, setOpSkipTime] = useState(null);
+  const [showSkipButton, setShowSkipButton] = useState(false);
+  const { skipTimes } = props;
+
+  const media = useMediaPlayer();
+  const { currentTime, duration } = useMediaStore();
+
+  useEffect(() => {
+    if (!media) return;
+
+    const findOp = skipTimes.find(
+      (time) => time.skipType?.toLowerCase() === "op"
+    );
+
+    if (!findOp) return;
+
+    setOpSkipTime(findOp);
+  }, [skipTimes]);
+
+  useEffect(() => {
+    if (!media) return;
+    if (!opSkipTime) return;
+
+    console.log("currentTime", currentTime);
+    console.log("opSkipTime.startTime", opSkipTime.interval?.startTime);
+
+    //show the skip button if the current time is greater than the start time of the op skip time and less than the end time of the op skip time
+    if (
+      currentTime > opSkipTime.interval?.startTime &&
+      currentTime < opSkipTime.interval?.endTime
+    ) {
+      setShowSkipButton(true);
+    } else {
+      setShowSkipButton(false);
+    }
+  }, [opSkipTime, currentTime]);
+
+  const handleSkip = () => {
+    if (!media) return;
+    if (!opSkipTime) return;
+    if (currentTime < opSkipTime.interval?.startTime) return;
+    media.currentTime = opSkipTime.interval?.endTime;
+  };
+
+  useHotkeys("s", () => {
+    handleSkip();
+  });
+
+  if (!showSkipButton) return null;
+  return (
+    <SkipButtonContainer onClick={handleSkip} title="hotkey: s">
+      Skip Opening
+    </SkipButtonContainer>
+  );
+};
+
+export default SkipButton;
--- src/components/Watch/controls/Slider/Slider.styles.js
+++ src/components/Watch/controls/Slider/Slider.styles.js
@@ -1,3 +1,4 @@
+import { rgba } from "polished";
 import styled from "styled-components";
 
 export const SliderWrapper = styled.div`
@@ -6,6 +7,15 @@ export const SliderWrapper = styled.div`
   align-items: center;
   justify-content: center;
   padding: 0 30px;
+`;
+
+export const OpSkipTime = styled.div`
+  position: absolute;
+  height: 4px;
+  background-color: ${({ theme }) => rgba("#c0b7eb", 1)};
+  left: ${(props) => props.fromLeft}%;
+  width: ${(props) => props.width}%;
+  z-index: 1;
 `;
 
 export const VideoTime = styled.h4``;
